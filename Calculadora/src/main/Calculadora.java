8762512a6b71f536f24be4263db791b94c816f73
Marcelo M. Borges <marcelomborges@outlook.com>
2020-11-12 10:24:42 -0300

    Maven - Gerando um novo projeto

---
+++ Calculadora/src/main/Calculadora.java
@@ -0,0 +1,13 @@
+public class Calculadora {
+        
+        int id;
+
+        public Calculadora (int id) {
+                this.id = id;
+        }
+
+        public int soma (int num1, int num2) {
+                return num1+num2;
+        }
+
+}
---
+++ Calculadora/src/test/CalculadoraTest.java
@@ -0,0 +1,8 @@
+public class CalculadoraTest {
+
+        public static void main(String[] args) {
+                Calculadora calc = new Calculadora(1);
+                int soma = calc.soma(5,5);
+                System.out.println(soma);
+        }
+}
